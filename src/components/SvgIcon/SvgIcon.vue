aa35ad4819fa440b455eec532eb5e249e0c275b5
Nikolay Lim <limnikolay99@gmail.com>
2024-11-13 00:58:25 +0300

    Реализован компонент для svg-иконок

---
+++ src/components/SvgIcon/SvgIcon.vue
@@ -0,0 +1,30 @@
+<script>
+import { h } from 'vue';
+
+const TAG_NAME_COMPONENT = 'svg';
+const TAG_NAME_INNER = 'use';
+
+const SvgIcon = (props, context) => {
+  return h(
+    TAG_NAME_COMPONENT,
+    {
+      ...context.attrs,
+      width: 24,
+      height: 24,
+      class: 'icon',
+      tabindex: '-1',
+      focusable: 'false',
+      'aria-hidden': 'true',
+    },
+    [
+      h(TAG_NAME_INNER, {
+        href: `#${props.name}`,
+      }),
+    ],
+  );
+};
+
+export default SvgIcon;
+</script>
+
+<style lang="scss" src="./SvgIcon.scss"></style>
