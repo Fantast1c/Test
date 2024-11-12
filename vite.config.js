7ca390d7f5535cb8f3f36867dc73b67c7234fad7
Nikolay Lim <limnikolay99@gmail.com>
2024-11-13 00:57:06 +0300

    Добавление scss и vite-plugin-svg-icons в проект

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
+    createSvgIconsPlugin({
+      // eslint-disable-next-line no-undef
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/')],
+      symbolId: '[name]',
+      inject: 'body-first',
+    }),
+
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
     },
   },
-})
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
