a4b19e3dade184dc1f680a02e0c9d129f2a62348
Nikolay Lim <limnikolay99@gmail.com>
2024-11-13 21:48:37 +0300

    Добавлен компонент BaseButton

---
+++ src/components/BaseButton/BaseButton.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  /*
+   * Свойство, отвечающее за атрибут type.
+   */
+  type: {
+    type: String,
+    default: 'button',
+    validator(value) {
+      return ['button', 'submit', 'reset'].includes(value);
+    },
+  },
+
+  /*
+   *Свойство, отвечающее за атрибут disabled.
+   */
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <button
+    :type="props.type"
+    :disabled="props.disabled"
+    class="button"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" src="./BaseButton.scss"></style>
