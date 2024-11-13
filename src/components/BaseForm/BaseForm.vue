a0451da71fd10199800608a6a4c4817e0866d691
Nikolay Lim <limnikolay99@gmail.com>
2024-11-13 23:53:47 +0300

    Добавление компонентов-оберток

---
+++ src/components/BaseForm/BaseForm.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <div class="form">
+    <p class="form__title">
+      <slot name="title"></slot>
+    </p>
+    <form class="form__content">
+      <slot name="default"></slot>
+    </form>
+  </div>
+</template>
+
+<style src="./BaseForm.scss"></style>
